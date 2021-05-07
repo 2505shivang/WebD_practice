d6439c1fc2e7256eee136e6ce5a3a35fb16b1616
2505shivang <2505shivang@gmail.com>
2021-05-07 12:46:37 +0530

    Basic http server

--- My_first_server(http)/index.js
+++ My_first_server(http)/index.js
@@ -1,8 +1,34 @@
 const http = require('http');
 const port = 8000;
+const fs = require('fs');
+const { profile } = require('console');
 
+function requestHandler(req,res){
+    res.writeHead(200, {'content-type': 'text/html'});
+    let filePath;
 
-const server = http.createServer();
+    switch(req.url){
+        case '/' : filePath = './index.html'
+        break;
+        case '/profile' : filePath = './profile.html'
+        break;
+        default :  filePath = './404.html';
+    }
+
+fs.readFile(filePath, function(err, data){
+    if(err){
+        console.log('err in filePath');
+        returnres.end();
+    }
+
+    return res.end(data);
+});
+
+    return;
+
+}
+
+const server = http.createServer(requestHandler);
 
 server.listen(port, function(err){
     if(err){
